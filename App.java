18aec3f645ac0c1803dae36c89a19a1fb22dba0b
Agasti Mhatre <56239538+agasti-mhatre@users.noreply.github.com>
2022-09-08 19:08:12 -0400

    Add files via upload

--- App.java
+++ App.java
@@ -3,20 +3,19 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        List<ContainerObj> list_of_Containers = new ArrayList<ContainerObj>(){
-            {
-                add(new ContainerObj(1, "a"));
-                add(new ContainerObj(2, "b"));
-                add(new ContainerObj(3, "c"));
-                add(new ContainerObj(4, "d"));
-                add(new ContainerObj(5, "e"));
-                add(new ContainerObj(6, "f"));
-                add(new ContainerObj(7, "g"));
-            }
-        };
+        List<Node> node_list = new ArrayList<Node>();
+
+        node_list.add(new Node(1));
+        node_list.add(new Node(2));
+        node_list.add(new Node(3));
+        node_list.add(new Node(4));
+        node_list.add(new Node(5));
         
-        
-        Hashmap x_hashmap = new Hashmap(list_of_Containers);
-        System.out.println(x_hashmap.table);
+        DoublyLinkedList x_DLL = new DoublyLinkedList(node_list);
+        x_DLL.pop();
+        x_DLL.add_node(new Node(6));
+
+        x_DLL.print_all_nodes();
+
     }
 }
